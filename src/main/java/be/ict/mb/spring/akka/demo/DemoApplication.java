9b4ab4a857a312cb76b3b589d6e60baf5a02432d
Marc Bogaerts <marcbogaerts@telenet.be>
2018-01-22 21:08:27 +0100

    Initial project

---
+++ src/main/java/be/ict/mb/spring/akka/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package be.ict.mb.spring.akka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
